daa79f000a867b392999655289a5ff0c06d39775
jony <sabinalatipova20@gmail.com>
2024-01-18 19:22:45 +0500

    home page

---
+++ components/Footer.tsx
@@ -0,0 +1,124 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface FooterProps {}
+
+const Footer: React.FC<FooterProps> = () => {
+   return (
+      <footer className="custom-container ">
+         <div className="mb-[50px] pt-[68px] px-[110px] rounded-[16px] shadow-[0px_5px_20px_0px_#15151526] bg-[#F5F5F5]">
+            <div className="flex items-center justify-between pb-[63px]">
+               <div className="">
+                  <h2 className="text-[50px] font-bold mb-4">
+                     Бесплатная консультация
+                  </h2>
+                  <p className="text-[#A3A2AB] max-w-[550px] text-[22px] font-medium mb-5 mr-20">
+                     Оставьте свои контакты и получите бесплатную консультацию
+                     по курсу, программе и спикерам.
+                  </p>
+                  <form className="max-w-[515px] grid grid-cols-2 gap-3">
+                     <input
+                        type="text"
+                        placeholder="Ваше имя"
+                        className="bg-white p-[17px] rounded-[9px] border border-[#E0E0E0] col-span-2"
+                     />
+                     <input
+                        type="text"
+                        placeholder="Телефон"
+                        className="bg-white p-[17px] rounded-[9px] border border-[#E0E0E0]"
+                     />
+                     <input
+                        type="text"
+                        placeholder="Откуда о нас узнали?"
+                        className="bg-white p-[17px] rounded-[9px] border border-[#E0E0E0]"
+                     />
+                     <button className="bg-[#151FE1] hover:bg-transparent border-[#151FE1] hover:text-[#151FE1] text-white text-lg font-bold py-4 border rounded-[7px] col-span-2 duration-150 ease-in">
+                        Отправить заявку
+                     </button>
+                  </form>
+               </div>
+               <div className="max-w-[400px]">
+                  <Image
+                     src={"/assets/images/tick.svg"}
+                     width={1000}
+                     height={1000}
+                     alt="tick"
+                  />
+               </div>
+            </div>
+            <div className="grid grid-cols-4 py-[63px] border-t border-[#1515151a]">
+               <div className="">
+                  <div className="max-w-[160px] mb-4">
+                     <Image
+                        src={"/assets/images/logo.svg"}
+                        width={1000}
+                        height={1000}
+                        alt="logo"
+                     />
+                  </div>
+                  <div className="">
+                     <p className="font-bold text-[#A3A2AB]">
+                        Профессиональное обучение профессиям будущего
+                     </p>
+                  </div>
+               </div>
+               <div className="">
+                  <h3 className="text-lg font-bold mb-5">Курсы:</h3>
+                  <ul className="text-[#A3A2AB] font-bold">
+                     <li className="mb-[6px]">
+                        <Link href={"#"}>Frontend-программирование</Link>
+                     </li>
+                     <li className="mb-[6px]">
+                        <Link href={"#"}>Мобильная разработка</Link>
+                     </li>
+                     <li className="mb-[6px]">
+                        <Link href={"#"}>SMM</Link>
+                     </li>
+                     <li>
+                        <Link href={"#"}>Графический дизайн</Link>
+                     </li>
+                  </ul>
+               </div>
+               <div className="">
+                  <h3 className="text-lg font-bold mb-5">Контакты:</h3>
+                  <ul className="text-[#A3A2AB] font-bold">
+                     <li className="text-black text-3xl font-bold mb-[21px]">
+                        <a href={"tel:+998 (90) 197-71-00"}>
+                           +998 (90) 197-71-00
+                        </a>
+                     </li>
+                     <li className="mb-[6px]">
+                        <Link href={"#"}>ул. Буюк Ипак Йули 52, Самарканд</Link>
+                     </li>
+                     <li>
+                        <Link href={"#"}>ул. Мирзо Улугбека, 35</Link>
+                     </li>
+                  </ul>
+               </div>
+               <div className="pl-14">
+                  <h3 className="text-lg font-bold mb-5">Социальные сети:</h3>
+                  <ul className="text-[#A3A2AB] font-bold">
+                     <li className="mb-[6px]">
+                        <Link href={"#"}>Instagram</Link>
+                     </li>
+                     <li>
+                        <Link href={"#"}>Telegram</Link>
+                     </li>
+                     <li>
+                        <Link href={"#"}>YouTube</Link>
+                     </li>
+                     <li>
+                        <Link href={"#"}>LinkedIn</Link>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div className="pt-[30px] pb-[38px] border-t border-[#1515151a]">
+               <p className="font-bold">MChJ “Wepro” © 2023</p>
+            </div>
+         </div>
+      </footer>
+   );
+};
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,91 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoIosMenu } from "react-icons/io";
+
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+   return (
+      <header className="shadow-sm">
+         <div className="custom-container py-4 flex gap-5 items-center justify-between">
+            <div className="flex items-center gap-11 max-3xl:gap-8 max-xl:gap-5">
+               <div className="max-w-[120px] max-xl:max-w-[100px] w-full">
+                  <Link href={"/"}>
+                     <Image
+                        src={"/assets/images/logo.svg"}
+                        width={1000}
+                        height={1000}
+                        alt="logo"
+                     />
+                  </Link>
+               </div>
+               <nav className="max-lg:hidden block">
+                  <ul className="flex items-center gap-6 max-3xl:gap-4 max-xl:gap-3">
+                     <li>
+                        <Link
+                           href={"#"}
+                           className="max-2xl:text-sm py-2 px-5 max-xl:px-3 rounded-full duration-150 ease-in bg-black hover:bg-black text-white hover:text-white"
+                        >
+                           Главная
+                        </Link>
+                     </li>
+                     <li>
+                        <Link
+                           href={"#"}
+                           className="max-2xl:text-sm py-2 px-5 max-xl:px-3 rounded-full duration-150 ease-in hover:bg-black hover:text-white"
+                        >
+                           Курсы и цены
+                        </Link>
+                     </li>
+                     <li>
+                        <Link
+                           href={"#"}
+                           className="max-2xl:text-sm py-2 px-5 max-xl:px-3 rounded-full duration-150 ease-in hover:bg-black hover:text-white"
+                        >
+                           Отзывы
+                        </Link>
+                     </li>
+                     <li>
+                        <Link
+                           href={"#"}
+                           className="max-2xl:text-sm py-2 px-5 max-xl:px-3 rounded-full duration-150 ease-in hover:bg-black hover:text-white"
+                        >
+                           Запись в группу
+                        </Link>
+                     </li>
+                  </ul>
+               </nav>
+            </div>
+
+            <div className="flex items-center gap-7 max-2xl:gap-5 max-sm:gap-3">
+               <p className="font-bold underline underline-offset-2 cursor-pointer">
+                  Рус
+               </p>
+               <Link
+                  href={"tel:+998 95 500-50-05"}
+                  className="font-bold underline underline-offset-2"
+               >
+                  <span className="max-3xl:hidden block">
+                     +998 95 500-50-05
+                  </span>
+                  <FaPhoneAlt
+                     className="max-3xl:block hidden"
+                     size={20}
+                     color="black"
+                  />
+               </Link>
+               <button className="bg-[#151FE1] hover:bg-transparent border-[#151FE1] hover:text-[#151FE1] text-white max-2xl:text-sm font-bold py-3 px-7 rounded-md border duration-150 ease-in max-lg:hidden block">
+                  Бесплатная консультация
+               </button>
+               <button className="bg-[#151FE1] hover:bg-transparent border-[#151FE1] hover:text-[#151FE1] text-white items-center gap-2 font-bold py-2 max-sm:py-1 px-4 max-sm:px-2 rounded-md border duration-150 ease-in max-lg:flex hidden">
+                  <IoIosMenu />
+                  Меню
+               </button>
+            </div>
+         </div>
+      </header>
+   );
+};
+
+export default Header;
