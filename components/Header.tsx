6ec0595ee4414fa32c13cb074c72b8c0c6317b62
jony <sabinalatipova20@gmail.com>
2024-02-01 20:38:59 +0500

    added modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,21 @@ import { usePathname } from "next/navigation";
 
 import { FaPhoneAlt } from "react-icons/fa";
 import { IoIosMenu } from "react-icons/io";
+import Modal from "./Modal";
+import { useState } from "react";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
    const pathName = usePathname();
+   const [isOpened, setIsOpened] = useState(false);
+
+   const onOpen = () => {
+      setIsOpened(true);
+   };
+   const onClose = () => {
+      setIsOpened(false);
+   };
 
    const links = [
       {
@@ -31,94 +41,103 @@ const Header: React.FC<HeaderProps> = () => {
    ];
 
    return (
-      <header
-         className={`${
-            pathName === "/course"
-               ? "bg-[#020119]"
-               : "border-b border-[#EEEBE0] bg-white"
-         }`}
-      >
-         <div className="custom-container py-4 flex gap-5 items-center justify-between">
-            <div className="flex items-center gap-11 max-3xl:gap-8 max-xl:gap-5">
-               <div className="max-w-[120px] max-xl:max-w-[100px] w-full">
-                  <Link href={"/"}>
-                     {pathName === "/course" ? (
-                        <Image
-                           src={"/assets/images/logo-white.svg"}
-                           width={1000}
-                           height={1000}
-                           alt="logo"
-                        />
-                     ) : (
-                        <Image
-                           src={"/assets/images/logo.svg"}
-                           width={1000}
-                           height={1000}
-                           alt="logo"
-                        />
-                     )}
-                  </Link>
+      <>
+         <Modal isOpened={isOpened} onClose={onClose} />
+         <header
+            className={`${
+               pathName === "/course"
+                  ? "bg-[#020119]"
+                  : "border-b border-[#EEEBE0] bg-white"
+            }`}
+         >
+            <div className="custom-container py-4 flex gap-5 items-center justify-between">
+               <div className="flex items-center gap-11 max-3xl:gap-8 max-xl:gap-5">
+                  <div className="max-w-[120px] max-xl:max-w-[100px] w-full">
+                     <Link href={"/"}>
+                        {pathName === "/course" ? (
+                           <Image
+                              src={"/assets/images/logo-white.svg"}
+                              width={1000}
+                              height={1000}
+                              alt="logo"
+                           />
+                        ) : (
+                           <Image
+                              src={"/assets/images/logo.svg"}
+                              width={1000}
+                              height={1000}
+                              alt="logo"
+                           />
+                        )}
+                     </Link>
+                  </div>
+                  <nav className="max-lg:hidden block">
+                     <ul className="flex items-center gap-6 max-3xl:gap-4 max-xl:gap-3">
+                        {links.map(
+                           (
+                              item: { title: string; link: string },
+                              idx: number
+                           ) => {
+                              return (
+                                 <li key={idx}>
+                                    <Link
+                                       href={item.link}
+                                       className={`max-2xl:text-sm py-2 px-5 max-xl:px-3 rounded-full duration-150 ease-in hover:bg-black ${
+                                          pathName === "/course"
+                                             ? "text-white"
+                                             : "hover:text-white"
+                                       }`}
+                                    >
+                                       {item.title}
+                                    </Link>
+                                 </li>
+                              );
+                           }
+                        )}
+                     </ul>
+                  </nav>
                </div>
-               <nav className="max-lg:hidden block">
-                  <ul className="flex items-center gap-6 max-3xl:gap-4 max-xl:gap-3">
-                     {links.map(
-                        (
-                           item: { title: string; link: string },
-                           idx: number
-                        ) => {
-                           return (
-                              <li key={idx}>
-                                 <Link
-                                    href={item.link}
-                                    className={`max-2xl:text-sm py-2 px-5 max-xl:px-3 rounded-full duration-150 ease-in hover:bg-black ${
-                                       pathName === "/course"
-                                          ? "text-white"
-                                          : "hover:text-white"
-                                    }`}
-                                 >
-                                    {item.title}
-                                 </Link>
-                              </li>
-                           );
-                        }
-                     )}
-                  </ul>
-               </nav>
-            </div>
 
-            <div className="flex items-center gap-7 max-2xl:gap-5 max-sm:gap-3">
-               <p
-                  className={`font-bold underline underline-offset-2 cursor-pointer ${
-                     pathName === "/course" ? "text-white" : "text-black"
-                  }`}
-               >
-                  Рус
-               </p>
-               <Link
-                  href={"tel:+998 95 500-50-05"}
-                  className={`font-bold underline underline-offset-2 ${
-                     pathName === "/course" ? "text-white" : "text-black"
-                  }`}
-               >
-                  <span className={`max-3xl:hidden block`}>
-                     +998 95 500-50-05
-                  </span>
-                  <FaPhoneAlt
-                     className="max-3xl:block hidden"
-                     size={20}
-                     color="black"
-                  />
-               </Link>
-               <button className="bg-[#151FE1] hover:bg-transparent border-[#151FE1] hover:text-[#151FE1] text-white max-2xl:text-sm font-bold py-3 px-7 rounded-md border duration-150 ease-in max-lg:hidden block">
-                  Бесплатная консультация
-               </button>
-               <button className="bg-[#151FE1] hover:bg-transparent border-[#151FE1] hover:text-[#151FE1] text-white items-center gap-2 font-bold py-2 max-sm:py-1 px-4 max-sm:px-2 rounded-md border duration-150 ease-in max-lg:flex hidden">
-                  <IoIosMenu />
-                  Меню
-               </button>
+               <div className="flex items-center gap-7 max-2xl:gap-5 max-sm:gap-3">
+                  <p
+                     className={`font-bold underline underline-offset-2 cursor-pointer ${
+                        pathName === "/course" ? "text-white" : "text-black"
+                     }`}
+                  >
+                     Рус
+                  </p>
+                  <Link
+                     href={"tel:+998 95 500-50-05"}
+                     className={`font-bold underline underline-offset-2 ${
+                        pathName === "/course" ? "text-white" : "text-black"
+                     }`}
+                  >
+                     <span className={`max-3xl:hidden block`}>
+                        +998 95 500-50-05
+                     </span>
+                     <FaPhoneAlt
+                        className="max-3xl:block hidden"
+                        size={20}
+                        color="black"
+                     />
+                  </Link>
+                  <button
+                     onClick={onOpen}
+                     className="bg-[#151FE1] hover:bg-transparent border-[#151FE1] hover:text-[#151FE1] text-white max-2xl:text-sm font-bold py-3 px-7 rounded-md border duration-150 ease-in max-lg:hidden block"
+                  >
+                     Бесплатная консультация
+                  </button>
+                  <button
+                     onClick={onOpen}
+                     className="bg-[#151FE1] hover:bg-transparent border-[#151FE1] hover:text-[#151FE1] text-white items-center gap-2 font-bold py-2 max-sm:py-1 px-4 max-sm:px-2 rounded-md border duration-150 ease-in max-lg:flex hidden"
+                  >
+                     <IoIosMenu />
+                     Меню
+                  </button>
+               </div>
             </div>
-         </div>
-      </header>
+         </header>
+      </>
    );
 };
 
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,55 +1,90 @@
+"use client";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
 
-interface ModalProps {}
+import { BiCloset } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
 
-const Modal: React.FC<ModalProps> = () => {
+interface ModalProps {
+   isOpened: boolean;
+   onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpened, onClose }) => {
+   useEffect(() => {
+      if (isOpened) {
+         document.body.style.overflowY = "hidden";
+      } else {
+         document.body.style.overflowY = "scroll";
+      }
+   }, [isOpened]);
+
    return (
-      <div className="fixed z-50 top-0 left-0 w-full h-screen backdrop-blur-sm bg-black/30">
-         <div className="max-w-xl w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-10 rounded-lg bg-white">
-            <div className="max-w-[160px] mb-3">
-               <Image
-                  src={"/assets/images/logo.svg"}
-                  width={1000}
-                  height={1000}
-                  alt="logo"
-               />
-            </div>
-            <div className="mb-5">
-               <h3 className="text-2xl text-black font-semibold">
-                  Заявка на консультацию
-               </h3>
-            </div>
-
-            <form>
-               <label className="flex flex-col mb-3">
-                  <span className="text-[#A3A2AB] text-[15px] mb-1">
-                     Ваши имя и фамилия
-                  </span>
-                  <input
-                     type="text"
-                     placeholder="Имя и фамилия"
-                     className="px-5 py-[18px] rounded-[9px] bg-[#F4F4F4]"
-                  />
-               </label>
-               <label className="flex flex-col mb-3">
-                  <span className="text-[#A3A2AB] text-[15px] mb-1">
-                     Ваши имя и фамилия
-                  </span>
-                  <input
-                     type="text"
-                     placeholder="Имя и фамилия"
-                     className="px-5 py-[18px] rounded-[9px] bg-[#F4F4F4]"
-                  />
-               </label>
-               <button className="w-full py-4 text-xl rounded-lg border-2 duration-150 ease-in text-white hover:text-[#151FE1] bg-[#151FE1] hover:border-[#151FE1] hover:bg-transparent">
-                  Отправить заявку
+      <AnimatePresence>
+         {isOpened && (
+            <motion.div
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 1 }}
+               exit={{ opacity: 0 }}
+               transition={{ duration: 0.3 }}
+               className={`w-full h-full flex items-center justify-center fixed z-50 top-0 left-0 backdrop-blur-sm bg-black/30`}
+               onClick={onClose}
+            >
+               <button
+                  onClick={onClose}
+                  className="absolute top-10 right-10 text-5xl"
+               >
+                  <IoClose color="white" />
                </button>
-            </form>
-            <div className="mt-4 py-3 border-t border-gray-400">
-               <p className="text-gray-400 text-sm">Пожалуйста, убедитесь, что правильно ввели данные.</p>
-            </div>
-         </div>
-      </div>
+               <div
+                  className="max-w-xl w-full p-10 rounded-lg bg-white"
+                  onClick={(e) => e.stopPropagation()}
+               >
+                  <div className="max-w-[160px] mb-3">
+                     <Image
+                        src={"/assets/images/logo.svg"}
+                        width={1000}
+                        height={1000}
+                        alt="logo"
+                     />
+                  </div>
+                  <div className="mb-5">
+                     <h3 className="text-2xl text-black font-semibold">
+                        Заявка на консультацию
+                     </h3>
+                  </div>
+
+                  <form>
+                     <label className="flex flex-col mb-3">
+                        <span className="text-[#A3A2AB] text-[15px] mb-1">
+                           Ваши имя и фамилия
+                        </span>
+                        <input
+                           type="text"
+                           placeholder="Имя и фамилия"
+                           className="px-5 py-[18px] rounded-[9px] bg-[#F4F4F4]"
+                        />
+                     </label>
+                     <label className="flex flex-col mb-3">
+                        <span className="text-[#A3A2AB] text-[15px] mb-1">
+                           Ваши имя и фамилия
+                        </span>
+                        <input type="text" placeholder="Имя и фамилия" />
+                     </label>
+                     <button className="w-full py-4 text-xl rounded-lg border-2 duration-150 ease-in text-white hover:text-[#151FE1] bg-[#151FE1] hover:border-[#151FE1] hover:bg-transparent">
+                        Отправить заявку
+                     </button>
+                  </form>
+                  <div className="mt-4 py-3 border-t border-gray-400">
+                     <p className="text-gray-400 text-sm">
+                        Пожалуйста, убедитесь, что правильно ввели данные.
+                     </p>
+                  </div>
+               </div>
+            </motion.div>
+         )}
+      </AnimatePresence>
    );
 };
 
