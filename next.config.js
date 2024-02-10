a835e2062127061a12761eb68cb2e46304793053
Daler <dsarifkulo021@gmail.com>
2024-02-10 12:01:49 +0500

    fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "main--stirring-twilight-81f674.netlify.app",
+                port: ''
+            }
+        ]
+    }
 };
 
 module.exports = nextConfig;
